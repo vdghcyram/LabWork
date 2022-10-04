fee8cfeae845314f5a4a395af9d8fd8bbff69f4c
vdghcyram <94733778+vdghcyram@users.noreply.github.com>
2022-10-04 22:07:33 +0300

    Добавление switch и создание первых функций

--- Chegodaeva_1/Chegodaeva_1.cpp
+++ Chegodaeva_1/Chegodaeva_1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void consol()
 {
+    int choice = 0;
     cout << "Меню:" << endl
         << "1. Добавить трубу" << endl
         << "2. Добавить КС" << endl
@@ -13,6 +14,18 @@ void consol()
         << "6. Сохранить" << endl
         << "7. Загрузить" << endl
         << "0. Выход" << endl;
+    cin >> choice;
+}
+
+void tube()
+{
+    double dlina;
+    cout << "Введите длину трубы:" << endl;
+    cin >> dlina;
+    do {
+        cout << "Введите корректное значение:";
+    } while (dlina < 0 || dlina == не буква)
+
 }
 
 int main()
@@ -20,4 +33,28 @@ int main()
     setlocale(LC_ALL, "rus");       //Поддержка русского языка
     consol();
     return 0;
+
+    switch ()
+        case 1:
+            tube();
+            break;
+        case 2:
+            ks();
+            break;
+        case 3:
+            all();
+            break;
+        case 4:
+            edit_tube();
+            break;
+        case 5:
+            edit_ks();
+            break;
+        case 6:
+            save();
+            break;
+        case 7:
+            load();
+            break;
+
 }
